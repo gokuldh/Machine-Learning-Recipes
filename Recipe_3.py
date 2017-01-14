d2a3d3867936c9c7f185e87ee07a6ef6ee46b1f3
Vardaan Sharma <gokuldh@gmail.com>
2017-01-14 19:32:02 +0530

    Recipe 3

---
+++ Recipe_3.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+greyhounds = 500
+labs = 500
+
+grey_height = 28 + 4 * np.random.randn(greyhounds)
+lab_height = 24 + 4 * np.random.randn(labs)
+
+# Greyounds - red, labradors - blue
+plt.hist([grey_height, lab_height], stacked=True, color=['r', 'b'])
+plt.show()
