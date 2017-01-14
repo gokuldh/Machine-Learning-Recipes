fbd8a21afc40ceb2dc80d2cdf25c4e17545f3b65
Vardaan Sharma <gokuldh@gmail.com>
2017-01-14 23:03:54 +0530

    Recipe 4

---
+++ Recipe_4.py
@@ -0,0 +1,36 @@
+# Import a dataset
+from sklearn import datasets
+
+iris = datasets.load_iris()
+
+X = iris.data  # features
+y = iris.target  # labels
+
+from sklearn.cross_validation import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
+
+# Classifier
+from sklearn import tree
+
+my_classifier = tree.DecisionTreeClassifier()
+my_classifier.fit(X_train, y_train)
+
+predictions = my_classifier.predict(X_test)
+print predictions
+
+from sklearn.metrics import accuracy_score
+
+print accuracy_score(y_test, predictions)
+
+from sklearn.neighbors import KNeighborsClassifier
+
+my_classifier = KNeighborsClassifier()
+my_classifier.fit(X_train, y_train)
+
+predictions = my_classifier.predict(X_test)
+print predictions
+
+from sklearn.metrics import accuracy_score
+
+print accuracy_score(y_test, predictions)
